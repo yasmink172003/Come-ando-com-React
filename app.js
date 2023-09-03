a23395c37de9e0d70e21217a65b76648140a93b0
Yasmin <126991883+yasmink172003@users.noreply.github.com>
2023-09-03 16:37:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+import react from'react';
+import logo from'logo.sveg';
+import 'app.css';
+
+function app(){
+        return(
+        <div className="App">
+        <header className="App-header">
+        <img src='react.jpg.png'></img>
+        <p>
+
+          Ola mundo
+        </p>
+        <a className='App-link'
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'
+        >
+         Ler Mais
+        </a>
+        </header>
+        </div>
+
+        );
+}
+  export default App;
